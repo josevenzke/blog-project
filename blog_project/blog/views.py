535d5d4847326a9f1502e41627645d1bf8296bab
josevenzke <josevenzkejr@gmail.com>
2020-12-11 07:36:45 -0300

    upload files

---
+++ blog_project/blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from blog.models import Post, Comment
+from django.views.generic import (TemplateView,ListView,DetailView)
+# Create your views here.
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+
+class PostDetailView(DetailView):
+    model = Post
