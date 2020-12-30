8eea4e5a7fb33931cb537ca87e9ce5ff28b09e2b
José Venzke <josevenzkejr@gmail.com>
2020-12-30 12:24:21 -0300

    Adding Pagination functionalities

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -9,6 +9,14 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
 
 class PostDetailView(DetailView):
     model = Post
